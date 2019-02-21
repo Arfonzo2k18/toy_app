75c4588ec7809efd5a6d331c8b22c27a4d0161f1
Arfonzo2k18 <alfonsomtb98@gmail.com>
2019-02-21 04:43:08 +0100

    Finalizada toy_app

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,7 +1,7 @@
 class Micropost
   include Mongoid::Document
+  belongs_to :user
   field :content, type: String
   field :user_id, type: String
   validates :content, length: { maximum: 140 }, presence: true
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User
   include Mongoid::Document
+  has_many :microposts
   field :name, type: String
   field :email, type: String
   validates :name, presence: true    # Replace FILL_IN with the right code.
   validates :email, presence: true    # Replace FILL_IN with the right code.
-
 end
